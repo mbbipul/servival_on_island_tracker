0ed2ce38b103c67cb5d073c3159a4658bc87ea84
Bipul Mandol <mbbipulmandol.cse3.bu@gmail.com>
2021-03-16 16:02:16 +0600

    Implement bing map using bing Map Sdk

--- app/src/main/java/com/example/survival_on_island/MainActivity.java
+++ app/src/main/java/com/example/survival_on_island/MainActivity.java
@@ -3,12 +3,73 @@ package com.example.survival_on_island;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.microsoft.maps.MapProjection;
+import com.microsoft.maps.MapRenderMode;
+import com.microsoft.maps.MapView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MapView mMapView;
+    FrameLayout bingMapLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bingMapLayout = findViewById(R.id.map_view); //
+
+        // initiate MapView instance
+        mMapView = new MapView(this, MapRenderMode.VECTOR);  // or use MapRenderMode.RASTER for 2D map
+        mMapView.setCredentialsKey(BuildConfig.BING_MAP_CREDENTIALS_KEY); // set bing map api key
+
+        bingMapLayout.addView(mMapView); // add Bing map view to frame
+
+        mMapView.onCreate(savedInstanceState);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mMapView.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mMapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mMapView.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+
 }
