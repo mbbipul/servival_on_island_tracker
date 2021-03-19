6a27acbfb946aab20cf80bee0862e6fdc8ccc9ba
Bipul Mandol <mbbipulmandol.cse3.bu@gmail.com>
2021-03-19 20:37:49 +0600

    Complete side navigation with login and logout functionalities

--- app/src/main/java/com/example/survival_on_island/MainActivity.java
+++ app/src/main/java/com/example/survival_on_island/MainActivity.java
@@ -1,36 +1,89 @@
 package com.example.survival_on_island;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.survival_on_island.ui.Home.DownLoadImageTask;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.microsoft.maps.GPSMapLocationProvider;
 import com.microsoft.maps.MapRenderMode;
 import com.microsoft.maps.MapUserInterfaceOptions;
 import com.microsoft.maps.MapUserLocation;
 import com.microsoft.maps.MapUserLocationTrackingState;
 import com.microsoft.maps.MapView;
-public class MainActivity extends AppCompatActivity {
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
     private static final String TAG = "LOG_FOR_MAIN_ACTIVITY";
     private static final int REQUEST_LOCATION_PERMISSION = 2345;
+    private static final int RC_SIGN_IN = 1245 ;
 
     private MapView mMapView;
     FrameLayout bingMapLayout;
+    SignInButton signInButton;
+    LinearLayout navProfileInfo ;
+    TextView userFullNameV;
+    TextView userEmaillV;
+    ImageView profileImageView;
+    MenuItem logoutMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        Menu navigationMenu = navigationView.getMenu();
+        logoutMenu = navigationMenu.findItem(R.id.logout);
+
+        signInButton = headerView.findViewById(R.id.sign_in_button);
+        navProfileInfo = headerView.findViewById(R.id.profile_info);
+        userFullNameV = headerView.findViewById(R.id.user_full_name);
+        userEmaillV = headerView.findViewById(R.id.user_email);
+        profileImageView = headerView.findViewById(R.id.profile_image);
+
+        signInButton.setOnClickListener(this);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+
+        toggle.syncState();
 
         bingMapLayout = findViewById(R.id.map_view); //
 
@@ -43,9 +96,6 @@ public class MainActivity extends AppCompatActivity {
 
         bingMapLayout.addView(mMapView); // add Bing map view to frame
         mMapView.onCreate(savedInstanceState);
-        showCurrentLocation();
-
-        requestLocationPermission();
 
     }
 
@@ -53,6 +103,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         mMapView.onStart();
+        updateUI();
+        showCurrentLocation();
+        requestLocationPermission();
     }
 
     @Override
@@ -109,6 +162,29 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Toast.makeText(this,"Login check on act",Toast.LENGTH_LONG);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            Toast.makeText(this,"Login check on act",Toast.LENGTH_LONG);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                updateUI();
+
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+
     // request permission at runtime
     private void requestLocationPermission() {
         if (ActivityCompat.checkSelfPermission(this,
@@ -151,4 +227,58 @@ public class MainActivity extends AppCompatActivity {
                 .show();
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                updateUI();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+        }
+    }
+
+    private void signIn() {
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+
+    }
+
+    private void updateUI() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null){
+            signInButton.setVisibility(View.GONE);
+            navProfileInfo.setVisibility(View.VISIBLE);
+            logoutMenu.setVisible(true);
+
+            new DownLoadImageTask(profileImageView).execute(String.valueOf(currentUser.getPhotoUrl()));
+
+            userFullNameV.setText(currentUser.getDisplayName());
+            userEmaillV.setText(currentUser.getEmail());
+            return;
+        }
+
+        signInButton.setVisibility(View.VISIBLE);
+        navProfileInfo.setVisibility(View.GONE);
+        logoutMenu.setVisible(false);
+        profileImageView.setImageResource(R.drawable.ic_user_icon);
+
+    }
 }
